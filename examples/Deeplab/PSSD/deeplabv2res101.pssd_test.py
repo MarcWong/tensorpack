f7a0971bc36488e388ee1033783904cc48c91c3f
marcWong <brucewayne@pku.edu.cn>
2018-05-21 03:23:46 +0800

    merge

--- examples/Deeplab/PSSD/deeplabv2res101.pssd_test.py
+++ examples/Deeplab/PSSD/deeplabv2res101.pssd_test.py
@@ -309,7 +309,8 @@ def proceed_test_dir(args):
     for i in tqdm(range(len(ll))):
         filename = ll[i]
         image = cv2.imread(os.path.join(args.test_dir,filename))
-        prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=CROP_SIZE, is_densecrf = False)
+
+	prediction = predict_scaler(image, mypredictor, scales=[0.5,0.75, 1, 1.25, 1.5], classes=CLASS_NUM, tile_size=CROP_SIZE, is_densecrf = False)
         prediction = np.argmax(prediction, axis=2)
         cv2.imwrite(os.path.join(final_dir,"{}".format(filename)), prediction)
         cv2.imwrite(os.path.join(visual_dir, "{}".format(filename)), np.concatenate((image, visualize_label(prediction)), axis=1))
@@ -368,7 +369,7 @@ if __name__ == '__main__':
     parser.add_argument('--output', help='fused output filename. default to out-fused.png')
     parser.add_argument('--validation', action='store_true', help='validate model on validation images')
     parser.add_argument('--test', default="true", action='store_true', help='generate test result')
-    parser.add_argument('--test_dir', default="/data1/dataset/dalitang/src", action='store_true', help='generate test result')
+    parser.add_argument('--test_dir', default="/home/wangyao/dataset/dalitang", action='store_true', help='generate test result')
     args = parser.parse_args()
     if args.gpu:
         os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
